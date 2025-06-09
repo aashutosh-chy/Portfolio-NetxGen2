d78bb8ef773eadbeb6f4ab886b87142cec65f901
Aashutosh Chaudhary <aashutoshchaudhary22@gmail.com>
2025-06-09 14:53:54 +0545

    Update SkillCard.tsx

--- components/skills/SkillCard.tsx
+++ components/skills/SkillCard.tsx
@@ -25,7 +25,7 @@ const Skill = ({ name, image }: skill) => {
                 className={"h-20 w-20 md:h-24 md:w-24 rounded-full bg-gray-100 dark:bg-grey-800 flex items-center justify-center"}>
                 <Image alt="skill" width={100} height={100} className={`h-12 w-12 md:h-14 md:w-14 object-contain ${theme === 'dark' && (name === "GitHub" || name === "Vercel" || name === "NextJS" || name === "ExpressJS" ? 'invert' : 'invert-0')}`} src={image} />
             </div>
-            <p className="text-sm md:text-base break-words max-w-[80px] md:max-w-[96px] truncate">{name}</p>
+            <p className="text-sm md:text-base text-center hyphens-auto">{name}</p>
         </div>
     )
 }
